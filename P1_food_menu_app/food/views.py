8da8b9fbd3748e8b59ef2a67e8eb1eaa0de72da5
Damian Jarzebowski <damian.r.hajduk@o2.pl>
2024-01-16 23:09:26 +0100

    wip

---
+++ P1_food_menu_app/food/views.py
@@ -0,0 +1,22 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Item
+from django.template import loader
+
+# Create your views here.
+def index(request):
+    item_list = Item.objects.all()
+    context = {
+        'item_list':item_list,
+    }
+    return render(request, 'food/index.html', context)
+
+def item(request):
+    return HttpResponse('<h1>This is an item viev<h1>')
+
+def detail(request, item_id):
+    item = Item.objects.get(pk=item_id)
+    context = {
+        'item':item,
+    }
+    return render(request, 'food/detail.html', context)
